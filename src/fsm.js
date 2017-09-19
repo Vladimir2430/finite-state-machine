7f9dcb4185d39bd924967550f2938dd9e07e4f4d
Vladimir <31570004+Vladimir2430@users.noreply.github.com>
2017-09-19 23:10:20 +0300

    Update fsm.js

--- src/fsm.js
+++ src/fsm.js
@@ -4,13 +4,7 @@ class FSM {
      * @param config
      */
     constructor(config) {
-        this.states=config.states;
-        this.curState=config.initial;
-        //history
-        this.histArray=[this.curState];
-        this.tailIndex=0;
-        this.wasCalled;
-        this.redoDisabled;
+        
     }
 
     /**
@@ -25,11 +19,7 @@ class FSM {
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {
-        if(this.states.hasOwnProperty(state)){
-            this.curState=state;
-            this.histArray.push(this.curState);
-            this.redoDisabled=true;
+    
         }
         else throw new Error();
     }
@@ -38,14 +28,7 @@ class FSM {
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {
-        for(let transition in this.states[this.curState].transitions){
-            if(transition===event){
-                this.curState=this.states[this.curState].transitions[event];
-                this.histArray.push(this.curState);
-                this.redoDisabled=true;
-                return;
-            }
+   
         }
         throw new Error();
     }
@@ -64,20 +47,9 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {
-        let ar=[];
-        if(arguments.length!=0){
-            for(let stateName in this.states){
-                if(this.states[stateName].transitions.hasOwnProperty(event)){
-                    ar.push(stateName);                }
-            }
+    
         }
-        else
-            for(let stateName in this.states){
-                ar.push(stateName);
-            }
-
-        return ar;
+        
     }
 
     /**
@@ -86,13 +58,7 @@ class FSM {
      * @returns {Boolean}
      */
     undo() {
-        if((this.histArray.length-this.tailIndex)>1){
-            this.tailIndex++;
-            this.curState=this.histArray[this.histArray.length-1-this.tailIndex];
-            this.redoDisabled=false;
-            return true;
-        }
-        else return false;
+        
     }
 
     /**
@@ -100,47 +66,17 @@ class FSM {
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {
-        if((!this.redoDisabled)&&(this.tailIndex>0)) {
-                this.tailIndex--;
-                this.curState=this.histArray[this.histArray.length-1-this.tailIndex];
-                return true;
-        }
-        else return false;
-    }
+    
 
     /**
      * Clears transition history
      */
-    clearHistory() {this.histArray=[];this.tailIndex=0;}
-}
+  
 
 module.exports = FSM;
 
 /** @Created by Uladzimir Halushka **/
-const config = {
-    initial: 'normal',
-    states: {
-        normal: {
-            transitions: {
-                study: 'busy',
-            }
-        },
-        busy: {
-            transitions: {
-                get_tired: 'sleeping',
-                get_hungry: 'hungry',
-            }
-        },
-        hungry: {
-            transitions: {
-                eat: 'normal'
-            },
-        },
-        sleeping: {
-            transitions: {
-                get_hungry: 'hungry',
-                get_up: 'normal',
+
             },
         },
     }
@@ -149,8 +85,6 @@ const config = {
 
 (function(){
 
-            const student = new FSM(config);
-
-            student.trigger('study');
-            console.log(student.getState()+'busy');
+           
+           
 })()
