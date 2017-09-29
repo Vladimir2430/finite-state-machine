0204f1262025a5fc619608267813f47c5cfcdd66
Vladimir <31570004+Vladimir2430@users.noreply.github.com>
2017-09-29 22:06:55 +0300

    Update fsm.js

--- src/fsm.js
+++ src/fsm.js
@@ -120,3 +120,39 @@ module.exports = FSM;
 
 /** @Created by Uladzimir Halushka **/
 
+const config = {
+    initial: 'normal',
+    aaa: {
+        normal: {
+            transitions: {
+                study: 'busy',
+            }
+        },
+        busy: {
+            transitions: {
+                get_tired: 'sleeping',
+                get_hungry: 'hungry',
+            }
+        },
+        hungry: {
+            transitions: {
+                eat: 'normal'
+            },
+        },
+        sleeping: {
+            transitions: {
+                get_hungry: 'hungry',
+                get_up: 'normal',
+            },
+        },
+    }
+};
+
+
+(function(){
+
+            const student = new FSM(config);
+
+            student.trigger('study');
+            console.log(student.getState()+'busy');
+})()
